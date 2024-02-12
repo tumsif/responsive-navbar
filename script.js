64c8c2d9acb4669ca4246eef6932e9cf731c40ac
Tumsifu Lema <tom455976@gmail.com>
2024-02-12 18:20:37 +0300

    finished a responsive navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+const sidemenu = document.querySelector(".sidebar");
+function showSidebar() {
+  sidemenu.style.display = "flex";
+}
+function closeSidebar() {
+  sidemenu.style.display = "none";
+}
